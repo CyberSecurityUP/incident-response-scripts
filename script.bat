97adb039ca7365a82c6890802c7e77d318a0f9c0
Joas A Santos <34966120+CyberSecurityUP@users.noreply.github.com>
2024-01-17 15:32:21 -0300

    Add files via upload

---
+++ script.bat
@@ -0,0 +1,22 @@
+@echo off
+
+:: Criando arquivo de log
+set LOGFILE=resultado_da_busca.log
+echo Resultados da Busca > %LOGFILE%
+
+:: Procurando arquivos TI.exe e Anydesk.exe em todas as unidades
+echo Procurando arquivos TI.exe e Anydesk.exe... >> %LOGFILE%
+for %%d in (C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
+    if exist %%d:\ (
+        dir /s /b %%d:\TI.exe >> %LOGFILE%
+        dir /s /b %%d:\Anydesk.exe >> %LOGFILE%
+    )
+)
+
+:: Procurando processos TI-WORKER.exe, Anydesk.exe e TI.exe
+echo Procurando processos TI-WORKER.exe, Anydesk.exe e TI.exe... >> %LOGFILE%
+tasklist | find "TI-WORKER.exe" >> %LOGFILE%
+tasklist | find "Anydesk.exe" >> %LOGFILE%
+tasklist | find "TI.exe" >> %LOGFILE%
+
+echo Busca concluída. Verifique o arquivo %LOGFILE% para os resultados.
